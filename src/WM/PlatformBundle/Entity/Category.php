dd2a5b704800cb824d480f19e2f3a2db47f39d00
maxou32 <31220874+maxou32@users.noreply.github.com>
2018-04-22 16:12:57 +0200

    reprise fonctinnement correct avant slug

--- src/WM/PlatformBundle/Entity/Category.php
+++ src/WM/PlatformBundle/Entity/Category.php
@@ -24,4 +24,25 @@ class Category
   private $name;
 
   // Getters et setters
+  /**
+   * @return int
+   */
+  public function getId()
+  {
+    return $this->id;
+  }
+  /**
+   * @param string $name
+   */
+  public function setName($name)
+  {
+    $this->name = $name;
+  }
+  /**
+   * @return string
+   */
+  public function getName()
+  {
+    return $this->name;
+  }
 }
--- src/WM/PlatformBundle/Entity/Skill.php
+++ src/WM/PlatformBundle/Entity/Skill.php
@@ -24,4 +24,26 @@ class Skill
   private $name;
   
   // Getters et setters
+  
+  /**
+   * @return integer
+   */
+  public function getId()
+  {
+    return $this->id;
+  }
+  /**
+   * @param string $name
+   */
+  public function setName($name)
+  {
+    $this->name = $name;
+  }
+  /**
+   * @return string
+   */
+  public function getName()
+  {
+    return $this->name;
+  }
 }
